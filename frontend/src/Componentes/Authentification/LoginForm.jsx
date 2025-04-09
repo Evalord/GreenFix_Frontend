b141aaa116c21ea135723e11e02196649651e842
Eveline Fotso <Sf656145023pgmail.com>
2025-04-09 17:00:59 +0100

    resolving errors

--- frontend/src/Componentes/Authentification/LoginForm.jsx
+++ frontend/src/Componentes/Authentification/LoginForm.jsx
@@ -12,48 +12,7 @@ const LoginForm = ({
   onSubmit,
   switchToRegister,
   onForgotPassword,
-// }) => (
-//   <div className='form-box login'>
-//     <form onSubmit={onSubmit}>
-//       <h1>Login</h1>
-//       <div className='input-box'>
-//         <input
-//           type='text'
-//           placeholder='Username or Email'
-//           required
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//         <FaUser className='icon' />
-//       </div>
-//       <div className='input-box'>
-//         <input
-//           type='password'
-//           placeholder='Password'
-//           required
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//         <FaLock className='icon' />
-//       </div>
-//       <div className='remember-forgot'>
-//         <label>
-//           <input type='checkbox' /> Remember me
-//         </label>
-//         <a href='#' onClick={onForgotPassword}>Forgot password?</a>
-//       </div>
-//       <button type='submit'>Login</button>
-//       <div className='register-link'>
-//         <p>
-//           Don't have an account?{" "}
-//           <a href='#' onClick={switchToRegister}>
-//             Register
-//           </a>
-//         </p>
-//       </div>
-//     </form>
-//   </div>
-// );
+
 }) => {
   const formik = useFormik({
     initialValues: {
--- frontend/src/Services/authService.jsx
+++ frontend/src/Services/authService.jsx
@@ -2,16 +2,16 @@ import axios from "axios";
 
 const API_URL = process.env.URL;
 
-export const loginUser = (email, password) => {
+const loginUser = (email, password) => {
   return axios.post(`${API_URL}/login`, { email, password });
 };
 
-export const registerUser = (username, email, password) => {
+const registerUser = (username, email, password) => {
   return axios.post(`${API_URL}/register`, { username, email, password });
 };
 
-export const sendResetPasswordLink = async (email) => {
+const sendResetPasswordLink = async (email) => {
   return await axios.post(`${API_URL}/auth/forgot-password`, { email })
 };
 
-export { loginUser, registerUser};
+export { loginUser, registerUser, sendResetPasswordLink};
